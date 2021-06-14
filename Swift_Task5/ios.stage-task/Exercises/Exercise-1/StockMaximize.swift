c76d67c4491ed015fc16ef2b9ddcd4dec653129d
Mikhail Kuzin <kuzin-ms@mail.ru>
2021-06-14 23:33:59 +0300

    feat: add solution of task 1 and some start of task 2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,36 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+
+        var profit: Int = 0
+        var wallet: Int = 0
+        var count: Int = 0
+        var i = 0
+        
+        func buy(_ b:Int) -> () {
+            wallet += b
+            count += 1
+        }
+        func sell(_ s:Int) -> () {
+            if count != 0 {
+            profit = (s * count) + profit - wallet
+            count = 0
+            wallet = 0
+            }
+        }
+        while i < prices.count - 1 {
+
+            if prices[i] <= prices[i+1] {
+                    buy(prices[i])
+            } else {
+                    sell(prices[i])
+            }
+            if prices[i+1] == prices.last {
+                    sell(prices[i+1])
+            }
+            i += 1
+        }
+    return profit
     }
 }
+
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,39 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+       
+        var finalMessage = ""
+        var haveMultiplier = false
+        
+        for char in message {
+          
+           var multiplier = 1
+        //   var charIndex = message.startIndex
+            
+            if char.isNumber && !haveMultiplier {
+                
+                multiplier = Int(char.description)!
+                haveMultiplier = true
+                var charIndex = char.self
+                
+                if multiplier == 0 {
+                    return ""
+                }
+            
+//                print("наличие множителя = \(haveMultiplier)")
+//                print("множитель = \(multiplier)")
+//                print(finalMessage)
+//                print("CharIndex = \(charIndex) \(type(of: charIndex))")
+            }
+            
+            if char.self != "[" && !char.isNumber && char.self != "]" {
+                
+                finalMessage.append(char.self)
+                print(finalMessage)
+            
+            }
+        }
+        print(finalMessage)
+        return String(finalMessage)
     }
 }
